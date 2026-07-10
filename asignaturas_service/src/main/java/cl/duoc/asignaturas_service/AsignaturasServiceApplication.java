1f82d2fc1f169cdf32653e0c821fbbb97a816bd1
NombreDeDesarrollador <mat.mirandag@duocuc.cl>
2026-07-10 01:58:28 -0400

    Falta docker, trello, presentacion

--- asignaturas_service/src/main/java/cl/duoc/asignaturas_service/AsignaturasServiceApplication.java
+++ asignaturas_service/src/main/java/cl/duoc/asignaturas_service/AsignaturasServiceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Ruta a través del Gateway")})
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AsignaturasServiceApplication {
